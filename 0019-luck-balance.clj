e873066fab1d3148c79e0da130b4786074f72aaf
qu4ku <qu4ku@hotmail.com>
2023-11-30 23:48:27 +0100

    luck-balance

---
+++ 0019-luck-balance.clj
@@ -0,0 +1,40 @@
+(ns luck-balance)
+
+
+;; https://www.hackerrank.com/challenges/luck-balance/
+;; dif: easy
+;; init 2023/11. done.
+
+(def input "6 3
+5 1
+2 1
+1 1
+8 1
+10 0
+5 0")
+
+(with-in-str input
+  (let [n (read)
+        k (read)
+        xs (for [_ (range n)]
+                   [(read) (read)])]
+    (println (-
+              (+
+               (->> xs
+                    (filter #(= 0 (% 1)))
+                    ((fn [x] (map #(% 0) x)))
+                    (reduce +))
+               (->> xs
+                    (filter #(= 1 (% 1)))
+                    ((fn [x] (map #(% 0) x)))
+                    sort
+                    reverse
+                    (take k)
+                    (reduce +)))
+              (->> xs
+                   (filter #(= 1 (% 1)))
+                   ((fn [x] (map #(% 0) x)))
+                   sort
+                   reverse
+                   (drop k)
+                   (reduce +))))))
