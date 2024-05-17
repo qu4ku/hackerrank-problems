bec4af5edfa94776abfea307f741e02dfcd3029d
qu4ku <qu4ku@hotmail.com>
2024-05-17 03:09:35 +0200

    clojure

---
+++ 0028-jim-and-the-orders.clj
@@ -0,0 +1,17 @@
+(ns jim-and-the-orders
+  (:require [clojure.string :as str]))
+
+;; https://www.hackerrank.com/challenges/jim-and-the-orders/
+;; dif: easy
+;; init 2024/05. done.
+
+
+(def orders [[1 3] [2 3] [3 3]])
+
+(defn solve [orders]
+  (->> orders
+       (map-indexed (fn [idx o] [(inc idx) (reduce + o)]))
+       (sort-by second)
+       (map first)))
+
+(solve orders)
